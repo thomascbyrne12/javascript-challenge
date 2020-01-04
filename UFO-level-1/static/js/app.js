0d033d726d8d170206544daf65d7e76608a9c59e
thomascbyrne12 <47927482+thomascbyrne12@users.noreply.github.com>
2020-01-04 13:54:14 -0500

    yea boi!

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,13 +1,18 @@
 // from data.js
 var tableData = data;
-data.forEach(function (tabledData) {
-    console.log(tableData;
-    var row = tbody.append("tr");
-    Object.entries(tableData).forEach(function ([key, value]) {
-        console.log(key, value);
-        // Append a cell to the row for each value
-        var cell = row.append("td");
-        cell.text(value);
-    });
+
+// from index.html
+var tbody = d3.select("tbody");
+
+// Logging data and writing to the table
+data.forEach(function(tableData) {
+  console.log(tableData);
+  var row = tbody.append("tr");
+  Object.entries(tableData).forEach(function([key, value]) {
+    console.log(key, value);
+    // Append a cell to the row for each value
+    var cell = row.append("td");
+    cell.text(value);
+  });
 });
 // YOUR CODE HERE!
